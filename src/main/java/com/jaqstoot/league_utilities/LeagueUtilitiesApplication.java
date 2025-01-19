71ffc042082a486cc7d2e90051bdb684f583646d
sardine fisherman <sardine@ps-1.sardines>
2025-01-18 20:05:36 -0600

    Initial commit

---
+++ src/main/java/com/jaqstoot/league_utilities/LeagueUtilitiesApplication.java
@@ -0,0 +1,13 @@
+package com.jaqstoot.league_utilities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeagueUtilitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeagueUtilitiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaqstoot/league_utilities/LeagueUtilitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaqstoot.league_utilities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeagueUtilitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
